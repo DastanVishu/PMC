2af05f93c020b0f5a5f23a076543ca53cf3a4338
vandanachadha <vishuprajapati15@gmail.com>
2023-07-27 14:55:34 +0530

    loading page is working now

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import {StyleSheet, View, Text, Image} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Base from "../components/pageBase";
+// import theme from '../../theme';
+
+const HomePage = () => {
+  return (
+    <Base>
+      <Text style={{color: "black"}}>hello</Text>
+    </Base>
+  );
+}
+
+
+export default HomePage
